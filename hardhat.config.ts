9ec32bee83fcfafc9587574e16898002ecb44b8c
DmitryD <dima.dyachkov@gmail.com>
2023-02-14 00:32:15 +0800

    Updated config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import {HardhatUserConfig, task} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-// import {ethers} from "hardhat";
+import "dotenv/config";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -10,7 +10,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-task("deploy", "Deploys NFT contract", async (taskArgs, hre) => {
+task(
+    "deploy",
+    "Deploys NFT contract",
+    async () => {
   // @ts-ignore
   const FarawayNFT = await ethers.getContractFactory("FarawayNFT");
   const nft = await FarawayNFT.deploy();
@@ -18,9 +21,9 @@ task("deploy", "Deploys NFT contract", async (taskArgs, hre) => {
   console.log(`Deployed FarawayNFT to ${nft.address}`);
 });
 
-const INFURA_API_KEY = "YOUR_INFURA_API_KEY";
-const GOERLI_PRIVATE_KEY = "YOUR_GOERLI_PRIVATE_KEY";
-const ETHERSCAN_API_KEY = "YOUR_ETHERSCAN_API_KEY";
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   etherscan: {
@@ -31,6 +34,7 @@ const config: HardhatUserConfig = {
   networks: {
     goerli: {
       url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      // @ts-ignore
       accounts: [GOERLI_PRIVATE_KEY],
     },
   },
